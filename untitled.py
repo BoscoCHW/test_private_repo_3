83ebc93d34dd2fc69b83253375a051084cde0115
boscochw_ubuntU_ideapad <bosco.chw@gmail.com>
2022-07-12 18:14:11 -0700

    c

--- untitled.py
+++ untitled.py
@@ -1,1 +1,11 @@
-kjnkzddfgs
+import subprocess
+
+
+def main():
+    process =subprocess.Popen(["git", "remote", "-v"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    output_text = output.decode("utf-8")
+    print(output_text)
+
+
+main()
